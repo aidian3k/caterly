5c5d12e48f70d9e8311c2c82087315029917a909
Stanleyowskiki <s.t.malinski@gmail.com>
2024-10-22 11:16:52 +0200

    Added front part

--- caterly-catering-company-frontend/src/index.tsx
+++ caterly-catering-company-frontend/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import NewMealForm from "./Components/NewMealForm";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <NewMealForm />
   </React.StrictMode>,
 );
 
