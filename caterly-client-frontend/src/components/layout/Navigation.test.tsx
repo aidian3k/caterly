f90ff578f2d567842ec24c7e15764b7c5e1bb01b
kwiatkm4 <01169585@pw.edu.pl>
2024-10-29 19:54:13 +0100

    Add some dummy test

---
+++ caterly-client-frontend/src/components/layout/Navigation.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Navigation from "./Navigation";
+import { BrowserRouter } from "react-router-dom";
+
+test("renders app logo", () => {
+  render(<Navigation />,{wrapper: BrowserRouter});
+  const titleElem = screen.getByText("Caterly");
+  expect(titleElem).toBeInTheDocument();
+});
