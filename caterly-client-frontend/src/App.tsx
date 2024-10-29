a02836ea5a021cc388e336669fe58d7dc56af320
kwiatkm4 <01169585@pw.edu.pl>
2024-10-29 19:17:13 +0100

    Add simple router

--- caterly-client-frontend/src/App.tsx
+++ caterly-client-frontend/src/App.tsx
@@ -1,26 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const tryAuthenticate = async () => {
+  // TODO: Sprawdza, czy użytkownik po uruchomieniu aplikacji jest już zalogowany
+  //  Jak tak, to dispatchuje login action w reduxie
+  return null;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <p>Logowanie</p>,
+    loader: tryAuthenticate, // TODO: jak zalogowany to przenies od razu do strony glownej
+  },
+  {
+    path: "/register",
+    element: <p>Rejestracja</p>,
+    loader: tryAuthenticate, // TODO: jak zalogowany to przenies od razu do strony glownej
+  },
+  {
+    path: "/",
+    element: <p>Layout</p>,
+    loader: tryAuthenticate,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
