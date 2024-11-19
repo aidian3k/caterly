363efa46b7f5126f144bf3cd22c0fc8e5ac354c5
Igor Kedzierawski <igor.kedzierawski@mail.pl>
2024-11-19 21:31:59 +0100

    lint

--- caterly-client-frontend/src/components/cart/CartApi.tsx
+++ caterly-client-frontend/src/components/cart/CartApi.tsx
@@ -1,19 +1,19 @@
 export type CartItem = {
-    id: number;
-    name: string;
-    quantity: number;
-    price: number;
+  id: number;
+  name: string;
+  quantity: number;
+  price: number;
 };
 
 /**Zwraca tablice z zawartością koszyka.*/
 export const getCart = (): CartItem[] => {
-    const storedCart = localStorage.getItem("cart");
-    return storedCart ? JSON.parse(storedCart) : [];
+  const storedCart = localStorage.getItem("cart");
+  return storedCart ? JSON.parse(storedCart) : [];
 };
 
 /**Nadpisuje koszyk nową zawartością.*/
 export const saveCart = (cart: CartItem[]): void => {
-    localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem("cart", JSON.stringify(cart));
 };
 
 /**Dodaje element do koszyka.
@@ -27,14 +27,16 @@ export const saveCart = (cart: CartItem[]): void => {
  *      });`
  */
 export const addItemToCart = (item: CartItem): void => {
-    const cart = getCart();
-    const existingItemIndex = cart.findIndex((cartItem) => cartItem.id === item.id);
-    if (existingItemIndex !== -1) {
-        cart[existingItemIndex].quantity += item.quantity;
-    } else {
-        cart.push(item);
-    }
-    saveCart(cart);
+  const cart = getCart();
+  const existingItemIndex = cart.findIndex(
+    (cartItem) => cartItem.id === item.id,
+  );
+  if (existingItemIndex !== -1) {
+    cart[existingItemIndex].quantity += item.quantity;
+  } else {
+    cart.push(item);
+  }
+  saveCart(cart);
 };
 
 /**Usuwa element z koszyka.
@@ -46,14 +48,14 @@ export const addItemToCart = (item: CartItem): void => {
  *      `removeItemFromCart(2, 5);`
  */
 export const removeItemFromCart = (id: number, quantity?: number): void => {
-    const cart = getCart();
-    const modifiedCart = cart
-        .map((item) => {
-            if (item.id === id) {
-                item.quantity = quantity === undefined ? 0 : item.quantity - quantity;
-            }
-            return item;
-        })
-        .filter((item) => item.quantity > 0);
-    saveCart(modifiedCart);
+  const cart = getCart();
+  const modifiedCart = cart
+    .map((item) => {
+      if (item.id === id) {
+        item.quantity = quantity === undefined ? 0 : item.quantity - quantity;
+      }
+      return item;
+    })
+    .filter((item) => item.quantity > 0);
+  saveCart(modifiedCart);
 };
