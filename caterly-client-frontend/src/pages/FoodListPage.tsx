6acd598fcfee7627b95afee4b255549ee535ee44
kwiatkm4 <147751735+kwiatkm4@users.noreply.github.com>
2024-11-25 16:09:11 +0100

    fix

--- caterly-client-frontend/src/pages/FoodListPage.tsx
+++ caterly-client-frontend/src/pages/FoodListPage.tsx
@@ -1,38 +1,8 @@
 import FoodCard from "../components/food-list/FoodCard";
 import useMeals from "../queries/meals.query";
-import CateringFoodEntity from "../interfaces/CateringFoodEntity";
 
 export default function FoodListPage() {
   const { data: meals, error, isError, isPending } = useMeals();
-  const testMeals: CateringFoodEntity[] = [
-    {
-      price: "12,34 zł",
-      typeOfFood: "Pies",
-      id: 1,
-      cateringEntity: {
-        city: "Wroclaw",
-        id: 2,
-      },
-    },
-    {
-      price: "12,34 zł",
-      typeOfFood: "Kurczak",
-      id: 6,
-      cateringEntity: {
-        city: "Lodz",
-        id: 5,
-      },
-    },
-    {
-      price: "31,22 zł",
-      typeOfFood: "Dog",
-      id: 3,
-      cateringEntity: {
-        city: "Warszawa",
-        id: 5,
-      },
-    },
-  ];
 
   if (isError) {
     console.log(error);
@@ -43,13 +13,13 @@ export default function FoodListPage() {
       <h2 className="font-bold text-2xl mb-3">Lista posiłków</h2>
       {isPending && <p>Ładowanie...</p>}
       {isError && <p>Wystąpił błąd!</p>}
-      {testMeals && (
+      {meals && (
         <div className="flex flex-col gap-4">
-          {testMeals.length === 0 ? (
+          {meals.length === 0 ? (
             <p>Brak posiłków</p>
           ) : (
             <>
-              {testMeals.map((el) => (
+              {meals.map((el) => (
                 <FoodCard
                   key={el.id}
                   price={el.price}
