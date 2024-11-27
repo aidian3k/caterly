44eaaa84dc98b49881ba1a2de70501e0916d6738
Monika Gardyjas <01169557@pw.edu.pl>
2024-11-27 18:58:51 +0100

    Update of changes that didn't save

--- caterly-catering-company-frontend/src/Components/MealService.tsx
+++
@@ -1,5 +0,0 @@
-import fetch from 'node-fetch';
-
-export const AddMeal = async function(name: string, description: string, price: number) {
-    return fetch("", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({name, description, price})})
-}
