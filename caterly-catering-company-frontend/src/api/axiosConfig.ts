518d329bb7ef838b72369ad2d377a95be3089035
Igor Kędzierawski <126198229+igorkedzierawski@users.noreply.github.com>
2024-11-29 10:01:43 +0100

    workaround fix for cors: make client calls against lh:8071 and lh:8072 for company (#105)
    Co-authored-by: Igor Kedzierawski <igor.kedzierawski@mail.pl>

--- caterly-catering-company-frontend/src/api/axiosConfig.ts
+++ caterly-catering-company-frontend/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/company",
+    baseURL: "http://localhost:8072/company",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
