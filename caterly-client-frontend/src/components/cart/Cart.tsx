f2b408204711fcb8019730d676ce7398bfa152bb
kwiatkm4 <147751735+kwiatkm4@users.noreply.github.com>
2024-11-30 17:17:16 +0100

    rework Cart to use redux

--- caterly-client-frontend/src/components/cart/Cart.tsx
+++ caterly-client-frontend/src/components/cart/Cart.tsx
@@ -1,54 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { getCart, removeItemFromCart } from "./CartApi";
-import CartItem from "../../interfaces/CartItem";
+import React, { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { removeCartItemAction } from "../../redux/actions/cartActions";
 
 const Cart = () => {
-  const [displayedCart, setDisplayedCart] = useState<CartItem[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const storedCart = getCart();
-    setDisplayedCart(storedCart);
-    setLoading(false);
-  }, []);
-
-  const calculateTotalPrice = (): number => {
-    return displayedCart.reduce(
-      (total, item) => total + item.quantity * item.price,
-      0,
-    );
-  };
-
-  if (loading) {
-    return <div>Ładowanie koszyka...</div>;
-  }
+  const cart = useSelector((state: RootState) => state.cart.cart);
+  const dispatch = useDispatch();
+  const totalPrice = useMemo(
+    () =>
+      Array.from(cart.values()).reduce(
+        (total, item) => total + item.quantity * item.price,
+        0,
+      ),
+    [cart],
+  );
 
   return (
     <div>
       <h1 className="font-bold text-2xl mb-3">Twój koszyk</h1>
-      {displayedCart.length === 0 ? (
+      {cart.size === 0 ? (
         <p>Twój koszyk jest pusty</p>
       ) : (
         <div>
           <ul>
-            {displayedCart.map((item) => (
+            {Array.from(cart.values()).map((item) => (
               <li key={item.id}>
                 {item.name} - {item.quantity} x {item.price} PLN ={" "}
                 {item.quantity * item.price} PLN
-                <button
-                  onClick={() => {
-                    removeItemFromCart(item.id);
-                    const storedCart = getCart();
-                    setDisplayedCart(storedCart);
-                  }}
-                >
+                <button onClick={() => dispatch(removeCartItemAction(item.id))}>
                   Usuń
                 </button>
               </li>
             ))}
           </ul>
           <p>
-            <strong>Łączna cena: {calculateTotalPrice()} PLN</strong>
+            <strong>Łączna cena: {totalPrice} PLN</strong>
           </p>
         </div>
       )}
