ffb3165bca01bc15487c4ee4c0f84e19b4d79c17
kwiatkm4 <147751735+kwiatkm4@users.noreply.github.com>
2024-11-30 18:28:16 +0100

    allow adding items to cart

--- caterly-client-frontend/src/pages/FoodListPage.tsx
+++ caterly-client-frontend/src/pages/FoodListPage.tsx
@@ -1,8 +1,29 @@
 import FoodCard from "../components/food-list/FoodCard";
 import useMeals from "../queries/meals.query";
+import CateringFoodEntity from "../interfaces/CateringFoodEntity";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { useNavigate } from "react-router-dom";
+import CartItem from "../interfaces/CartItem";
+import { addItemToCartAction } from "../redux/actions/cartActions";
 
 export default function FoodListPage() {
   const { data: meals, error, isError, isPending } = useMeals();
+  const cart = useSelector((state: RootState) => state.cart.cart);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleAdd = (item: CateringFoodEntity): void => {
+    const price = parseFloat(item.price);
+    const cartItem: CartItem = {
+      id: item.id,
+      name: item.typeOfFood,
+      price: !isNaN(price) ? price : 0,
+      quantity: 1,
+    };
+
+    dispatch(addItemToCartAction(cartItem));
+    navigate("/cart");
+  };
 
   if (isError) {
     console.log(error);
@@ -25,6 +46,9 @@ export default function FoodListPage() {
                   price={el.price}
                   typeOfFood={el.typeOfFood}
                   city={el.cateringEntity.city}
+                  inCart={cart.some((item) => item.id === el.id)}
+                  onCheckInCart={() => navigate("/cart")}
+                  onAddToCart={() => handleAdd(el)}
                 />
               ))}
             </>
