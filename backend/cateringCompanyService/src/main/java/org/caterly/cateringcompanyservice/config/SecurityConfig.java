cec3633eca6ea43e3d49b2c6dc6a38725bf6d47c
Igor Kędzierawski <126198229+igorkedzierawski@users.noreply.github.com>
2024-12-05 09:29:00 +0100

    added spring security and configured cors (#109)
    * added spring security and configured cors
    
    * added explicit permit for all requests
    
    * allowed communication for clientFE-companyBE and companyFE-clientBE
    
    * Fix feign client API url
    
    ---------
    
    Co-authored-by: Igor Kedzierawski <igor.kedzierawski@mail.pl>
    Co-authored-by: kwiatkm4 <147751735+kwiatkm4@users.noreply.github.com>

--- backend/cateringCompanyService/src/main/java/org/caterly/cateringcompanyservice/config/SecurityConfig.java
+++ backend/cateringCompanyService/src/main/java/org/caterly/cateringcompanyservice/config/SecurityConfig.java
@@ -1,15 +1,11 @@
-package org.caterly.cateringclientservice.config;
+package org.caterly.cateringcompanyservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
@@ -29,36 +25,13 @@ public class SecurityConfig {
     @Bean
     public static SecurityFilterChain
     securityFilterChain(final HttpSecurity http) throws Exception {
-        http
+        return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/client/auth/login",
-                                "/client/auth/register").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(
-                                SessionCreationPolicy.IF_REQUIRED)
-                        .sessionFixation().migrateSession()
-                )
-                .securityContext(securityContext ->
-                        securityContext.securityContextRepository(
-                                new HttpSessionSecurityContextRepository())
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/client/auth/logout")
-                        .logoutSuccessHandler(
-                                (request, response,
-                                 authentication) -> response
-                                        .setStatus(HttpStatus.OK.value()))
-                );
-        return http.build();
-    }
-
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+                .build();
     }
 
     @Bean
