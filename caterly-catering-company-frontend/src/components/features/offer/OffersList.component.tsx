1709f2db7ca0bad8246e026289f0ae8379f83b81
kwiatkm4 <147751735+kwiatkm4@users.noreply.github.com>
2024-12-06 11:14:37 +0100

    redirect to edit page on click

--- caterly-catering-company-frontend/src/components/features/offer/OffersList.component.tsx
+++ caterly-catering-company-frontend/src/components/features/offer/OffersList.component.tsx
@@ -1,32 +1,42 @@
 import React from "react";
-import {CateringOfferDTO} from "../../../interfaces/offer/CateringOfferDTO";
+import { CateringOfferDTO } from "../../../interfaces/offer/CateringOfferDTO";
 import OfferItem from "./OfferItem.component";
 import { DeleteFoodRequest } from "../../../interfaces/offer/DeleteFoodRequest";
+import { useNavigate } from "react-router-dom";
 
 interface OfferListProps {
-    offers: CateringOfferDTO[];
-    onDeleteOffer: (deleteRequest: DeleteFoodRequest) => Promise<void>;
+  offers: CateringOfferDTO[];
+  onDeleteOffer: (deleteRequest: DeleteFoodRequest) => Promise<void>;
 }
 
 const OffersList: React.FC<OfferListProps> = ({ offers, onDeleteOffer }) => {
+  const navigate = useNavigate();
+  const handleDelete = async (id: number) => {
+    const deleteRequest = new DeleteFoodRequest(id);
 
-    const handleDelete = async (id: number) => {
-        const deleteRequest = new DeleteFoodRequest(id);
+    try {
+      await onDeleteOffer(deleteRequest);
+    } catch (error) {
+      console.error("Failed to delete offer:", error);
+    }
+  };
 
-        try {
-            await onDeleteOffer(deleteRequest); 
-        } catch (error) {
-            console.error("Failed to delete offer:", error);
-        }
-    };
+  const handleEdit = (id: number) => {
+    navigate(`/offer/${id}`);
+  };
 
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-            {offers.map((offer) => (
-                <OfferItem key={offer.id} offer={offer} onDelete={handleDelete}/>
-            ))}
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+      {offers.map((offer) => (
+        <OfferItem
+          key={offer.id}
+          offer={offer}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default OffersList;
