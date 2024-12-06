4661e4d99df08041e3c1c8d15a01d5f840b600b1
kwiatkm4 <147751735+kwiatkm4@users.noreply.github.com>
2024-12-06 20:52:04 +0100

    fetch offer details

---
+++ caterly-catering-company-frontend/src/api/react-query/query/offer/offer-details.query.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../../../axiosConfig";
+import { CateringOfferDTO } from "../../../../interfaces/offer/CateringOfferDTO";
+
+export default function useOfferDetails(
+  cateringCompanyId?: string,
+  foodId?: string,
+) {
+  return useQuery({
+    queryKey: ["offer", cateringCompanyId, foodId],
+    queryFn: async () => {
+      const res = await axiosInstance.get<CateringOfferDTO>(
+        `/api/offers/${cateringCompanyId}/${foodId}`,
+      );
+
+      return res.data;
+    },
+    staleTime: Infinity,
+  });
+}
--- caterly-catering-company-frontend/src/pages/CateringCompanyEditOfferPage.component.tsx
+++ caterly-catering-company-frontend/src/pages/CateringCompanyEditOfferPage.component.tsx
@@ -1,11 +1,20 @@
 import { useParams } from "react-router-dom";
+import useOfferDetails from "../api/react-query/query/offer/offer-details.query";
 
 export default function CateringCompanyEditOfferPage() {
   const { cateringCompanyId, foodId } = useParams();
+  const {
+    data: offer,
+    error,
+    isError,
+    isPending,
+  } = useOfferDetails(cateringCompanyId, foodId);
 
   return (
-    <p>
-      Firma {cateringCompanyId}, Posiłek: {foodId}
-    </p>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Edytuj posiłek</h1>
+      {isPending && <p>Ładowanie...</p>}
+      {isError && <p>Wystąpił błąd: {error.message ?? "Nieznany błąd"}</p>}
+    </div>
   );
 }
