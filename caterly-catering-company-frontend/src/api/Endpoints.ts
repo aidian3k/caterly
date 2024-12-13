c2dea0cfed9c97bd3c4542ebe0f1247c368edfca
Monika Gardyjas <01169557@pw.edu.pl>
2024-12-13 21:46:05 +0100

    Company Show Orders FE: - added ORDERS to Endpoints.ts and query function - added CateringOrderDTO - added components OrderItem, OrdersList - created page CateringCompanyOrdersPage - added routing and link in navigation

--- caterly-catering-company-frontend/src/api/Endpoints.ts
+++ caterly-catering-company-frontend/src/api/Endpoints.ts
@@ -3,4 +3,5 @@ export enum Endpoints {
     OFFER = "/api/offers/:cateringCompanyId",
     OFFER_ADD= "/api/offers",
     FOOD = "/api/offers/:cateringCompanyId/:foodId",
+    ORDERS = "/api/orders/:cateringCompanyId",
 }
---
+++ caterly-catering-company-frontend/src/components/features/orders/OrdersList.component.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { CateringOrderDTO } from "../../../interfaces/order/CateringOrderDTO";
+import OrderItem from "./OrderItem.component";
+import { useNavigate } from "react-router-dom";
+
+interface OrderListProps {
+  orders: CateringOrderDTO[];
+}
+
+const OrdersList: React.FC<OrderListProps> = ({ orders }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="p-10">
+      <div className="flex flex-row items-center justify-between w-full p-10 border-b-2 border-black-500">
+        <p className="text-lg text-gray-600"> Numer zamówienia </p>
+        <p className="text-lg text-gray-600"> Data </p>
+        <p className="text-lg text-gray-600"> Status </p>
+        <p className="text-lg text-gray-600"> Opcje </p>
+      </div>
+      {orders.map((order) => (
+        <div className="gap-6 p-4">
+          <OrderItem
+            key={order.id}
+            order={order}
+          />
+        </div> 
+      ))}
+    </div>
+  );
+};
+
+export default OrdersList;
