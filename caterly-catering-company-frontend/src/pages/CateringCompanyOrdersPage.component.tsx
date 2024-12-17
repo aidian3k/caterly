70e9b2cdc2533d634cc40e22e984070f117a3b23
Monika Gardyjas <96150849+gardyjam@users.noreply.github.com>
2024-12-17 23:16:07 +0100

    Update caterly-catering-company-frontend/src/pages/CateringCompanyOrdersPage.component.tsx
    Co-authored-by: Mateusz <147751735+kwiatkm4@users.noreply.github.com>

--- caterly-catering-company-frontend/src/pages/CateringCompanyOrdersPage.component.tsx
+++ caterly-catering-company-frontend/src/pages/CateringCompanyOrdersPage.component.tsx
@@ -11,7 +11,7 @@ const CateringCompanyOrdersPage = () => {
 
             {
                 isPending && (
-                    <p>Loading...</p>
+                    <p>Ładowanie...</p>
                 )
             }
             {
